e5188a5281177cb319fa5105fd54a153544f91df
Alexander Klymuk <swcAlexander@gmail.com>
2023-07-10 10:45:57 +0300

    add full version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Section } from 'components/Section/Section.jsx';
 import { Statistics } from 'components/Statistics/Statistics.jsx';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions.jsx';
-import { Notification } from 'components/Notifications/Notifications';
 
-export class App extends React.Component {
-  static defaultProps = {
-    goodInitialValue: 0,
-    neutralInitialValue: 0,
-    badInitialValue: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = state => {
+    switch (state) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  static propTypes = {
-    // тут описуємо пропи
-  };
-  // state = {
-  //     value: this.props.initialValue,
-  // }
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  const countTotalFeedback = () => good + neutral + bad;
 
-  onLeaveFeedback = state => {
-    this.setState(prevState => ({ [state]: prevState[state] + 1 }));
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     return Math.round((good / total) * 100) || 0;
   };
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    const totalPercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
-    return (
-      <div
-        className="feedback"
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          padding: '20px',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          ></FeedbackOptions>
+  const totalFeedback = countTotalFeedback();
+  const totalPercentage = countPositiveFeedbackPercentage();
+  const options = ['good', 'neutral', 'bad'];
+
+  return (
+    <div
+      className="feedback"
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '20px',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      {totalFeedback !== 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={totalPercentage}
+          />
         </Section>
-        {totalFeedback !== 0 ? (
-          <Section title="Statistics">
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={totalFeedback}
-              positivePercentage={totalPercentage}
-            ></Statistics>
-          </Section>
-        ) : (
-          <Section title="Statistics">
-            <Notification></Notification>
-          </Section>
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Section title="Statistics">
+          <p>No feedback given</p>
+        </Section>
+      )}
+    </div>
+  );
+};
+
+export default App;
